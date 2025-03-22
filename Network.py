bdf5fca36bb08f8780dec6fd5df179992d940301
Balakalki <99853151+Balakalki@users.noreply.github.com>
2025-03-22 15:51:45 +0530

    Update Network.py

--- Network.py
+++ Network.py
@@ -153,27 +153,3 @@ class Network:
           total_error+=error
           self.backwardPass(targets, learning_rate)
         print(f"Epoch {epoch + 1} / {epochs}, Error: {total_error/len(input_datas)}")
-
-
-# data collection
-data = pd.read_csv('/content/drive/MyDrive/ml_lab/MLAssignment/final_data_64_64.csv')
-# print(data)
-
-Y = data.iloc[:, :1].values.flatten()
-X = data.iloc[:, 1:].values
-
-max_number = max(max(sublist) for sublist in X)
-result_list = [[element / max_number for element in sublist] for sublist in X]
-# print(result_list[0][0])
-from sklearn.model_selection import train_test_split
-xtrain, xtest, ytrain, ytest = train_test_split(result_list,Y, test_size=0.3, random_state=42)
-
-
-# network creation
-nLayers=int(input("enter number of layers"))
-obj=Network(nLayers)  #object for Network class
-obj.Create_Network(4096)  #creates a network by creating layers and appending to Layers list in Network class (this takes input length as argument)
-
-
-# obj.fit(flatten_inputs, ytrain, 1000, 0.1)
-# print(obj.Layers[-1].output)
